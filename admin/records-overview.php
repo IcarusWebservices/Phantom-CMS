cc651c77ab1a3fba403d7ebe9ed3bb30b26b7a36
PXJesse <jessetraas@gmail.com>
2020-02-10 23:25:30 +0100

    Added button styling

--- admin/records-overview.php
+++ admin/records-overview.php
@@ -21,7 +21,8 @@ admin_template($title, $menu, function() {
     global $title, $type;
 ?>
     <h1><?= $title ?></h1>
-    <a href="<?= uri_resolve('/admin/record?mode=new&type=' . $type) ?>" style="text-decoration: underline; color: blue;">New post</a>
+    <br>
+    <a href="<?= uri_resolve('/admin/record?mode=new&type=' . $type) ?>" class="button">New post</a>
     
     <table id="records-table">
         <thead>
