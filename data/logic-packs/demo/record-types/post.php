463154f9dd669dd474bd8898f1fb027da967a041
Icarus Websolutions <47674100+IcarusWS@users.noreply.github.com>
2020-02-11 17:33:18 +0100

    Please work

--- data/logic-packs/demo/record-types/post.php
+++ data/logic-packs/demo/record-types/post.php
@@ -16,13 +16,13 @@ class Demo_Post_Record_Type extends PH_Record_Type {
     public function saveRecord($rawEditorData, $previousRecord = null)
     {
         if($previousRecord) {
-            $content = isset($rawEditorData["content:textfield"]) ? $rawEditorData["content:textfield"] : null;
+            $content = isset($rawEditorData["content:textfield"]) ? $rawEditorData["content:textfield"] : "Geen content";
             $previousRecord->content = $content;
             // $previousRecord->title = $rawEditorData["system:title"];
 
             return $previousRecord;
         } else {
-            $content = isset($rawEditorData["content:textfield"]) ? $rawEditorData["content:textfield"] : null;
+            $content = isset($rawEditorData["content:textfield"]) ? $rawEditorData["content:textfield"] : "Geen content";
             $newRecord = new PH_Record([
                 "title" => $rawEditorData["system:title"],
                 "content" => $content
