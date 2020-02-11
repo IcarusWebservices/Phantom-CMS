0da61ba8c159f9d1260c8dd619fea25fe97de054
Jesse <44534584+PXJesse@users.noreply.github.com>
2020-02-11 16:47:59 +0100

    Removed demo js

--- data/themes/umbra/js/main.js
+++ data/themes/umbra/js/main.js
@@ -1,13 +1,5 @@
 const banner = document.querySelector('.banner');
 const navbar = document.querySelector('.navbar');
-const MenuButton = document.getElementById('hamburger-expand');
-const Menu = document.getElementById('navbarBasicExample');
-
-MenuButton.addEventListener('click', (e) => {
-    if(Menu.style.display == "none") Menu.style.display = "block";
-    else if(!Menu.style.display) Menu.style.display = "block";
-    else Menu.style.display = "none";
-})
 
 if (banner) {
   navbar.classList.add('transparent');
