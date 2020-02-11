19c481514d37fd892c3a90661a4becf33351e1aa
Icarus Websolutions <47674100+IcarusWS@users.noreply.github.com>
2020-02-11 17:30:29 +0100

    Quick fix

--- data/logic-packs/demo/record-types/post.php
+++ data/logic-packs/demo/record-types/post.php
@@ -10,21 +10,7 @@ class Demo_Post_Record_Type extends PH_Record_Type {
 
     public function processContent($content)
     {
-        $json = json_decode($content);
-
-        if($json) {
-
-            $post_format_version = [1,0,0];
-
-            if(isset($json->formatVersion)) {
-                $post_format_version = $json->formatVersion;
-            }
-
-            $content = $json->content;
-
-        } else {
-            return false;
-        }
+        return $content;
     }
 
     public function saveRecord($rawEditorData, $previousRecord = null)
