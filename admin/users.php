b90cb5f6c833d841e81f6dcdf963b3ffb87bfbd6
Icarus Websolutions <47674100+IcarusWS@users.noreply.github.com>
2020-02-11 13:35:52 +0100

    Added "add user" and "delete user"

--- admin/users.php
+++ admin/users.php
@@ -6,7 +6,29 @@ login_required();
 admin_template("Users", $menu, function() {
     $users = PH_Query::users([]);
 ?>
+<div id="user-creation" class="modal">
+
+<!-- Modal content -->
+<div class="modal-content">
+  <div class="modal-header">
+    <span class="close">&times;</span>
+    <h2>Add user</h2>
+  </div>
+  <div class="modal-body">
+    <form id="newuserform">
+        <p>Username: <input type="text" id="username" name="username" autocomplete="nope"></p>
+        <p>Email: <input type="email" id="email" name="email"></p>
+        <p>Password: <input type="password" id="password" autocomplete="new-password" name="password"></p>
+    </form>
+  </div>
+  <div class="modal-footer">
+    <h3><a href="#" id="createuser">Create</a></h3>
+  </div>
+</div>
+
+</div>
 <h1>Users</h1>
+<a href="#" class="button" id="newuseropen">New user</a>
 <table id="records-table">
     <thead>
         <tr>
@@ -26,7 +48,7 @@ admin_template("Users", $menu, function() {
                         <?php
                             // var_dump(session()->user->id);
                             if(session()->user->id != $user->id) {
-                                ?><a href="<?= uri_resolve('/admin/actions/user?action=delete&id=' . $user->id) ?>" class="link">Delete</a><?php
+                                ?><a data-delete-id="<?= $user->id ?>" class="link delete-user" href="#">Delete</a><?php
                             } else echo "(Cannot delete self)";
                         ?>
                         
@@ -37,5 +59,7 @@ admin_template("Users", $menu, function() {
         ?>
     </tbody>
 </table>
+<script src="<?= uri_resolve('/admin/js/ajax.js') ?>"></script>
+<script src="<?= uri_resolve('/admin/js/users.js') ?>"></script>
 <?php
 }, "collection:settings", "users");
