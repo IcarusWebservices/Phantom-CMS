1655b1330986c284105e2ef22bf58061b11555ad
PXJesse <jessetraas@gmail.com>
2020-02-26 12:53:40 +0100

    Updated error pages
    Added an "Under Construction" page and redesigned the 404 page

--- data/themes/nova/404.php
+++ data/themes/nova/404.php
@@ -7,19 +7,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?= uri_resolve('/core/pages/css/landing.css') ?>">
-    <title>This page was not found on this server - <?= get_string('website_name') ?></title>
+    <link rel="stylesheet" href="<?= uri_resolve('/data/themes/nova/css/error.css') ?>">
+    <title>This page was not found - <?= get_string('website_name') ?></title>
 </head>
 <body>
-    <div id="container">
-        <div id="logo">
-            <h1>...!</h1>
-        </div>
-        <h1>This page was not found on this server</h1>
-        <p>
-            The uri that you entered was not found on this server (/<?= $request->request_uri ?>)
-        </p>
-    </div>
-    <span id="version">Nova Theme</span>
+    <main>
+        <section class="error-grid">
+            <div class="error-illustration">
+                <img class="error-illustration-img" src="<?= uri_resolve('/data/themes/nova/img/error_illustration_512.jpg') ?>" alt="Error">
+            </div>
+            <div class="error">
+                <h1>404</h1>
+                <p>The page you requested was not found on this server (<?= $request->request_uri ?>)</p>
+                <br>
+                <a href="<?= uri_resolve('/wim') ?>" class="button">Take me home</a><br>
+                or <a href="#" class="link">report this</a>
+            </div>
+        </section>
+    </main>
 </body>
 </html>
