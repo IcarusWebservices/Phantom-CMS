ea3f1c12ef82e2cc9a2287a81188702e849f71e4
Icarus Websolutions <47674100+IcarusWS@users.noreply.github.com>
2020-03-01 18:24:41 +0100

    Fixed get_template_part

--- core/includes/displayengine/ph-theme.php
+++ core/includes/displayengine/ph-theme.php
@@ -18,9 +18,25 @@ function get_template_part($relative_path, $data = null) {
     global $theme_folder, $theme_valid;
 
     if($theme_valid) {
-        if(file_exists(DATA . 'themes/' . $theme_folder . '/template-parts/' . $relative_path)) {
+
+        $spl = explode(':', $theme_folder);
+
+        if(count($spl)>1) {
+
+            $project = $spl[0];
+            $theme = $spl[1];
+
+            $path = DATA . 'projects/' . $project . '/themes/' . $theme . '/template-parts/' . $relative_path;
+
+        } else {
+
+            $path = DATA . 'themes/' . $theme_folder . '/template-parts/' . $relative_path;
+
+        }
+
+        if(file_exists($path)) {
             $data = $data;
-            include DATA . 'themes/' . $theme_folder . '/template-parts/' . $relative_path;
+            include $path;
         }
     }
 
